733d38bdbed2fcb3fa81488495db1c00d2a98236
Destiny Serna <destinyeserna@gmail.com>
2020-11-16 16:29:39 -0800

    Initial Commit

---
+++ A Word Game/A_Word_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Word_GameApp.swift
+//  A Word Game
+//
+//  Created by Destiny Serna on 11/16/20.
+//
+
+import SwiftUI
+
+@main
+struct A_Word_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
